5bd8c4f32f44585276f1685a91b931376690bc20
copyninja1004 <wendyhotdog101@gmail.com>
2023-06-22 07:15:31 -0400

    fixed the some bug

--- lib/views/auth/login_view.dart
+++ lib/views/auth/login_view.dart
@@ -12,7 +12,7 @@ class LoginPage extends StatelessWidget {
         child: Column(
           children: [
             Container(
-                padding: EdgeInsets.only(top: 150),
+                padding: const EdgeInsets.only(top: 150),
                 child: Column(children: [
                   Image.asset(
                     'assets/logo/logo_white.png',
@@ -22,49 +22,67 @@ class LoginPage extends StatelessWidget {
               padding: const EdgeInsets.only(top: 50, left: 40, right: 40),
               child: Column(children: [
                 TextFormField(
-                  decoration:
-                      const InputDecoration(label: Center(
-                        child: Text('Email or Username', style: TextStyle(fontFamily: "Radley", fontSize: 18, color: Colors.black),),
-                      )),
+                  decoration: const InputDecoration(
+                      label: Center(
+                    child: Text(
+                      'Email or Username',
+                      style: TextStyle(
+                          fontFamily: "Radley",
+                          fontSize: 18,
+                          color: Colors.black),
+                    ),
+                  )),
                 ),
                 TextFormField(
-                  decoration: const InputDecoration(label: Center(
-                    child: Text('Password', style: TextStyle(fontFamily: "Radley", fontSize: 18, color: Colors.black)),
+                  decoration: const InputDecoration(
+                      label: Center(
+                    child: Text('Password',
+                        style: TextStyle(
+                            fontFamily: "Radley",
+                            fontSize: 18,
+                            color: Colors.black)),
                   )),
                   obscureText: true,
                 ),
               ]),
             ),
             Container(
-              padding: EdgeInsets.only(top: 30, left: 40, right: 40),
+              padding: const EdgeInsets.only(top: 30, left: 40, right: 40),
               child: Column(
                 children: [
                   ElevatedButton(
                       onPressed: () {/* Add login logic here */},
                       style: ElevatedButton.styleFrom(
-                          primary: Colors.black,
-                          minimumSize: const Size.fromHeight(40)
-                      ),
-                      child: Text('Sign in', style: TextStyle(fontFamily: "Radley", fontSize: 18))
-                  ),
+                          backgroundColor: Colors.black,
+                          minimumSize: const Size.fromHeight(40)),
+                      child: const Text('Sign in',
+                          style:
+                              TextStyle(fontFamily: "Radley", fontSize: 18))),
                   TextButton(
-                    onPressed: () => Navigator.pushNamed(context, '/forget-password'),
-                    child: const Text('Forget Password?', style: TextStyle(fontFamily: "Radley", fontSize: 16, color: Colors.black)),
+                    onPressed: () =>
+                        Navigator.pushNamed(context, '/forget-password'),
+                    child: const Text('Forget Password?',
+                        style: TextStyle(
+                            fontFamily: "Radley",
+                            fontSize: 16,
+                            color: Colors.black)),
                   )
                 ],
               ),
             ),
             Container(
-              padding: EdgeInsets.only(top: 30, left: 40, right: 40),
+              padding: const EdgeInsets.only(top: 30, left: 40, right: 40),
               child: Column(
                 children: [
                   ElevatedButton(
-                      onPressed: () => Navigator.pushNamed(context, '/register'),
+                      onPressed: () =>
+                          Navigator.pushNamed(context, '/register'),
                       style: ElevatedButton.styleFrom(
-                          primary: Colors.black,
-                          minimumSize: const Size.fromHeight(40)
-                      ),
-                      child: const Text('Sign up', style: TextStyle(fontFamily: "Radley", fontSize: 18))),
+                          backgroundColor: Colors.black,
+                          minimumSize: const Size.fromHeight(40)),
+                      child: const Text('Sign up',
+                          style:
+                              TextStyle(fontFamily: "Radley", fontSize: 18))),
                 ],
               ),
             )
