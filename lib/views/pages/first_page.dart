2bbf2f9ddb0a7befc5be5049d8b42183fcfef9fd
copyninja1004 <wendyhotdog101@gmail.com>
2023-06-23 11:42:55 -0400

    complete the 3 pages

--- lib/views/pages/first_page.dart
+++ lib/views/pages/first_page.dart
@@ -6,15 +6,15 @@ class FirstPage extends StatelessWidget {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     return Scaffold(
-      // appBar: AppBar(
-      //   backgroundColor: Colors.white,
-      //   leading: IconButton(
-      //     onPressed: () => Navigator.pushNamed(context, '/forget-password'),
-      //     icon: const Icon(Icons.search, color: Colors.black),
-      //   ),
-      //   title: Image.asset('assets/logo/small_logo.png'),
-      //   centerTitle: true,
-      // ),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () => Navigator.pushNamed(context, '/forget-password'),
+          icon: const Icon(Icons.search, color: Colors.black),
+        ),
+        title: Image.asset('assets/logo/small_logo.png'),
+        centerTitle: true,
+      ),
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: false,
         showUnselectedLabels: false,
@@ -29,78 +29,78 @@ class FirstPage extends StatelessWidget {
         },
         items: [
           BottomNavigationBarItem(
-              label: "Home",
-              icon: Image.asset('assets/img/home.png')
-          ),
+              label: "Home", icon: Image.asset('assets/img/home.png')),
           BottomNavigationBarItem(
-            label: "Multi-User",
-              icon: Image.asset('assets/img/user.png')
-          ),
+              label: "Multi-User", icon: Image.asset('assets/img/user.png')),
           BottomNavigationBarItem(
-              label: "Plus",
-              icon: Image.asset('assets/img/plus.png')
-          ),
+              label: "Plus", icon: Image.asset('assets/img/plus.png')),
           BottomNavigationBarItem(
-              label: "Alram",
-              icon: Image.asset('assets/img/alram.png')
-          ),
+              label: "Alram", icon: Image.asset('assets/img/alram.png')),
           BottomNavigationBarItem(
-              label: "User",
-              icon: Image.asset('assets/img/multi_user.png')
-          )
+              label: "User", icon: Image.asset('assets/img/multi_user.png'))
         ],
       ),
       body: SingleChildScrollView(
         padding: EdgeInsets.only(top: 50),
-        child: Column(
-
-            children: [
-              Image.asset('assets/img/banner2.png', width: width, fit: BoxFit.fitWidth,)
-              // Container(
-              //     padding: const EdgeInsets.only(top: 22.88, left: 20, bottom: 20),
-              //     child: const Row(
-              //         mainAxisAlignment: MainAxisAlignment.start,
-              //         children: [
-              //           Text("Suggested", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, fontFamily: 'Montserrat'))
-              //         ])
-              // ),
-              // Container(
-              //   padding: const EdgeInsets.only(left: 12, right: 12),
-              //   height: 160,
-              //   child:GridView.builder(
-              //       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              //         crossAxisCount: 3,
-              //       ),
-              //       itemCount: 3,
-              //       itemBuilder: (BuildContext context, int index) {
-              //         return Container(
-              //           width: 103,
-              //           height: 171,
-              //           color: const Color(0xFFE9E8E8),
-              //           margin: EdgeInsets.only(left: 5, right: 5),
-              //           padding: const EdgeInsets.only(top: 20),
-              //           child: Column(
-              //             crossAxisAlignment: CrossAxisAlignment.center,
-              //             children: [
-              //               Container(
-              //                 alignment: Alignment.center,
-              //                 child: Image.asset('assets/img/model.png'),
-              //               ),
-              //               const SizedBox(height: 10,),
-              //               const Text("rachelmorrison", style: TextStyle(fontFamily: "Montserrat", fontSize: 12)),
-              //             ],
-              //           ),
-              //           // Image.asset('assets/img/model.png');
-              //         );
-              //       }
-              //   ),
-              // ),
-              // Image.asset('assets/img/background.png', width: width, fit: BoxFit.fitWidth,),
-              // Image.asset('assets/img/banner.png', width: width, fit: BoxFit.fitWidth,)
-            ]
-        ),
+        child: Column(children: [
+          // Image.asset('assets/img/banner2.png', width: width, fit: BoxFit.fitWidth,)
+          Container(
+              padding: const EdgeInsets.only(top: 22.88, left: 20, bottom: 20),
+              child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text("Suggested",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Montserrat'))
+                  ])),
+          Container(
+            padding: const EdgeInsets.only(left: 12, right: 12),
+            height: 160,
+            child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                ),
+                itemCount: 3,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    width: 103,
+                    height: 171,
+                    color: const Color(0xFFE9E8E8),
+                    margin: EdgeInsets.only(left: 5, right: 5),
+                    padding: const EdgeInsets.only(top: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          alignment: Alignment.center,
+                          child: Image.asset('assets/img/model.png'),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const Text("rachelmorrison",
+                            style: TextStyle(
+                                fontFamily: "Montserrat", fontSize: 12)),
+                      ],
+                    ),
+                    // Image.asset('assets/img/model.png');
+                  );
+                }),
+          ),
+          Image.asset(
+            'assets/img/background.png',
+            width: width,
+            fit: BoxFit.fitWidth,
+          ),
+          Image.asset(
+            'assets/img/banner.png',
+            width: width,
+            fit: BoxFit.fitWidth,
+          )
+        ]),
       ),
-
     );
   }
 }
